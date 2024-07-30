a884165cb317fa655092d051b3fd4336275b9337
Riccardo Bolzoni <r.bolzoni2@campus.unimib.it>
2024-07-30 14:54:14 +0200

    added new exercises and updates old ones

---
+++ working/deckmodule/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='DeckModule',
+      description='deck and card class for the scientific computing course',
+      author='Riccardo Bolzoni',
+      author_email='r.bolzoni2@campus.unimib.it',
+      version='0.1',
+      packages=find_packages(),
+      install_requires=[])
